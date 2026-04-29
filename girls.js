bc4a1e306348dc46b4c5e9121fd6a9e7f4c71c67
Rana Magdy <ranamagdy5899@gmail.com>
2026-04-29 13:29:41 +0300

    rana added dark mode

--- girls.js
+++ girls.js
@@ -13,8 +13,33 @@ images.forEach(function (img) {
     });
 });
 
-let darkBtn = document.querySelector(".theme-toggle");
+const themeBtn = document.getElementById('themetoggle');
+const icon = document.getElementById('themeicon');
 
-darkBtn.addEventListener("click", function () {
-    document.body.classList.toggle("dark-mode");
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+    icon.classList.replace('fa-moon', 'fa-sun');
+
+}
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    let theme = 'light';
+
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.replace('fa-moon', 'fa-sun');
+
+        theme = 'dark';
+    } else {
+        icon.classList.replace('fa-sun', 'fa-moon');
+        icon.style.color = "";
+        theme = 'light';
+    }
+
+
+    localStorage.setItem('theme', theme);
 });
