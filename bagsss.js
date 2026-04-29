cf0859933a83649b429391a57c2c4ba2459bfe37
Rana Magdy <ranamagdy5899@gmail.com>
2026-04-29 21:08:50 +0300

    rana changed

--- bagsss.js
+++ bagsss.js
@@ -1,1 +1,26 @@
+let buttons = document.querySelectorAll(".add-to-cart");
 
+buttons.forEach((btn) => {
+    btn.addEventListener("click", function () {
+
+        let product = btn.closest(".product-card");
+
+        let name = product.querySelector("h3").textContent.trim();
+        let price = product.querySelector(".price-bag").textContent.trim();
+        let image = product.querySelector("img").src;
+
+        let item = {
+            name: name,
+            price: price,
+            image: image
+        };
+
+        let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+        cart.push(item);
+
+        localStorage.setItem("cart", JSON.stringify(cart));
+
+        alert("Added to cart");
+    });
+});
