f82c4ef5406f32ce8de1257ea74d735d5d7f3ac8
Nada <nadaaboda2006@gmail.com>
2026-05-01 02:47:36 +0200

    hehe

--- sports_script.js
+++ sports_script.js
@@ -20,3 +20,35 @@ buttons.forEach((btn) => {
         alert("Added to cart!");
     });
 });
+    
+
+const themeBtn = document.getElementById('themetoggle');
+const icon = document.getElementById('themeicon');
+
+
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+    document.body.classList.add('dark-mode');
+    icon.classList.replace('fa-moon', 'fa-sun');
+
+}
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+
+    let theme = 'light';
+
+    if (document.body.classList.contains('dark-mode')) {
+        icon.classList.replace('fa-moon', 'fa-sun');
+
+        theme = 'dark';
+    } else {
+        icon.classList.replace('fa-sun', 'fa-moon');
+        icon.style.color = "";
+        theme = 'light';
+    }
+
+
+    localStorage.setItem('theme', theme);
+});
